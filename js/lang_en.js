e9654d2080772401f642b0b5a89aaa85a558ef7a
Flauxo <flauxo@hotmail.com>
2026-05-12 14:59:53 +0200

    Update English newspaper name to The Daily Rigor

--- js/lang_en.js
+++ js/lang_en.js
@@ -41,7 +41,7 @@ I18n.register('en', {
 'hub.no_appointments': 'No appointments yet...', 'hub.eternal_rest': 'ETERNAL REST',
 'hub.funeral_house': 'FUNERAL HOUSE', 'hub.motto': '"We care to the end"',
 'hub.news_btn': 'DAILY NEWS',
-'hub.news_title': 'THE RIGOR MORTIS',
+'hub.news_title': 'THE DAILY RIGOR',
 'hub.welcome': '☠ Welcome, {0}. The dead await.',
 'hub.locked': '🔒 Unlock this improvement in the Upgrades menu!',
 // Nav
